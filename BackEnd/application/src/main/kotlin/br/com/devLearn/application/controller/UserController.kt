b4687a2efd92d176d2de8310bcadfb5d02037ad4
Matheus Eduardo Leite Marcolino <matheuselm@ciandt.com>
2022-05-18 17:10:51 -0300

    Postgres fixed

--- BackEnd/application/src/main/kotlin/br/com/devLearn/application/controller/UserController.kt
+++ BackEnd/application/src/main/kotlin/br/com/devLearn/application/controller/UserController.kt
@@ -1,11 +1,17 @@
 package br.com.devLearn.application.controller
 
 import br.com.devLearn.application.service.UserService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
 class UserController(private val service: UserService) {
+
+    @GetMapping
+    fun imprime(): String {
+        return "aaaaaaa"
+    }
 
 }
