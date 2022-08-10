3259e67f46f692d9ec184bb8aea71aa6b0d3ba22
Matheus Eduardo Leite Marcolino <matheuselm@ciandt.com>
2022-08-10 16:58:38 -0300

    Proxy Fix

---
+++ BackEnd/UrlAuthorize/src/main/kotlin/com/devlearn/urlauthorize/UrlauthorizeApplication.kt
@@ -0,0 +1,11 @@
+package com.devlearn.urlauthorize
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlauthorizeApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrlauthorizeApplication>(*args)
+}
